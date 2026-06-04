d13cac428ce04e1099482a4c152f8dc40e19b707
Abdulhakeem Sakhel <abodhakeemfabl@gmail.com>
2026-06-04 22:40:13 +0300

    starting ddt for WF2

--- src/test/java/abd/coinmarketcap/ConverterPageTest.java
+++ src/test/java/abd/coinmarketcap/ConverterPageTest.java
@@ -1,8 +1,15 @@
 package abd.coinmarketcap;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ConverterPageTest extends BaseTest {
@@ -26,6 +33,33 @@ public class ConverterPageTest extends BaseTest {
         driver.get(BASE_URL + getPath());
     }
     
+    @DataProvider(name = "searchDataProvider")
+    public Object[][] getSearchData() throws IOException {
+        List<Object[]> data = new ArrayList<>();
+        String csvFile = "src/test/resources/converter_ddt_data.csv";
+        String line;
+
+        try (BufferedReader bufferReader = new BufferedReader(new FileReader(csvFile))) {
+            bufferReader.readLine(); // skip header
+            while ((line = bufferReader.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    continue;
+                }
+                String[] row = line.split(",", -1);
+
+                data.add(new Object[]{
+                    row[0].trim(), //Test case ID
+                    row[1].trim(), //From Currency
+                    row[2].trim(), //to Currency
+                    row[3].trim(), //amount
+                    row[4].trim(), // ExpectedBehavior
+                    row[5].trim(), // Description
+                });
+            }
+        }
+        return data.toArray(new Object[0][]);
+    }
+
     @Test
     public void convertCryptoToFiat() {
         String fromCurrency = "Bitcoin";
