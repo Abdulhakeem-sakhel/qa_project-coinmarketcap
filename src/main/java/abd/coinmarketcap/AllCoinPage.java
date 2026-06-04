ee2cf9f33872fb40663faa53b97aed594d4d3124
Abdulhakeem Sakhel <abodhakeemfabl@gmail.com>
2026-06-05 01:02:13 +0300

    starting

--- src/main/java/abd/coinmarketcap/AllCoinPage.java
+++ src/main/java/abd/coinmarketcap/AllCoinPage.java
@@ -1,5 +1,31 @@
 package abd.coinmarketcap;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class AllCoinPage {
-    
+    // any column cell have a unique class even in a different layout
+    private By marketCapCells = new By.ByCssSelector("span.sc-11478e5d-1");
+    private By priceCells = new By.ByCssSelector("div.sc-631098c-0.ilZTOW");
+
+    private By marketCapSortingToggleBy = new By.ByXPath("//p[contains(@class, 'llNEXf') and contains(text(), 'Market Cap')]");
+    private By priceSortingToggleBy = new By.ByXPath("//p[contains(@class, 'llNEXf') and contains(text(), 'Price')]");
+
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public AllCoinPage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void loadTheTable() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
 }
