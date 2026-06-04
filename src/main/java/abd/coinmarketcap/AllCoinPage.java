884799163bba9696b403bc770ee4e1f758b57565
Abdulhakeem Sakhel <abodhakeemfabl@gmail.com>
2026-06-05 02:01:34 +0300

    small fix

--- src/main/java/abd/coinmarketcap/AllCoinPage.java
+++ src/main/java/abd/coinmarketcap/AllCoinPage.java
@@ -13,8 +13,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllCoinPage {
     // any column cell have a unique class even in a different layout
-    private By marketCapCells = new By.ByCssSelector("span.sc-11478e5d-1");
-    private By priceCells = new By.ByCssSelector("div.sc-631098c-0.ilZTOW");
+    private By marketCapCellsBy = new By.ByCssSelector("span.sc-11478e5d-1");
+    private By priceCellsBy = new By.ByCssSelector("div.sc-631098c-0.ilZTOW");
 
     private By marketCapSortingToggleBy = new By.ByXPath("//p[contains(@class, 'llNEXf') and contains(text(), 'Market Cap')]");
     private By priceSortingToggleBy = new By.ByXPath("//p[contains(@class, 'llNEXf') and contains(text(), 'Price')]");
@@ -37,21 +37,21 @@ public class AllCoinPage {
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
         while (true) {
-            final int before = driver.findElements(marketCapCells).size();
-            List<WebElement> rows = driver.findElements(marketCapCells);
+            final int before = driver.findElements(marketCapCellsBy).size();
+            List<WebElement> rows = driver.findElements(marketCapCellsBy);
             js.executeScript("arguments[0].scrollIntoView(true);", rows.get(rows.size() - 1));
             try {
-                wait.until(driver -> driver.findElements(marketCapCells).size() > before);
+                wait.until(driver -> driver.findElements(marketCapCellsBy).size() > before);
             } catch (Exception e) {
                 break;
             }
         }
         js.executeScript("window.scrollTo(0, 0);");
-        return driver.findElements(marketCapCells).size();
+        return driver.findElements(marketCapCellsBy).size();
     }
 
     public boolean checkMarketCapColumnOrderDes(int numberOfRows) {
-        List<WebElement> marketCapCells = driver.findElements(marketCapSortingToggleBy);
+        List<WebElement> marketCapCells = driver.findElements(marketCapCellsBy);
         int leftIndex = -1;
         int rightIndex = -1;
 
@@ -69,7 +69,36 @@ public class AllCoinPage {
             if (rightValue > leftValue) {
                 return false;
             }
+            leftIndex++;
+            rightIndex++;
         }
         return true;
+    }
+
+    public void togglePriceOrdering() {
+
+    }
+
+    public boolean checkPriceCapColumnOrderDes(int numberOfRows) {
+        List<WebElement> priceCells = driver.findElements(priceCellsBy);
+        int leftIndex = -1;
+        int rightIndex = -1;
+
+        if (priceCells.size() == numberOfRows + 1) {
+            leftIndex = 1; // the first index should be an advertisement skip it
+            rightIndex = 2;
+        } else {
+            leftIndex = 0;
+            rightIndex = 1;
+        }
+
+        while (leftIndex < priceCells.size() - 1) {
+            long leftValue = parseNumber(priceCells.get(leftIndex).getText());
+            long rightValue = parseNumber(priceCells.get(rightIndex).getText());
+            if (rightValue > leftValue) {
+                return false;
+            }
+        }
+        return true;    
     }
 }
